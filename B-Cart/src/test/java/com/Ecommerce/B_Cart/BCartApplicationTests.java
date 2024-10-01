e5230dd4678a0970bee7d6f15250f6eb7244c895
bhavesh <bhaveshgupta13524@gmail.com>
2024-10-01 15:46:22 +0530

    intial project

---
+++ B-Cart/src/test/java/com/Ecommerce/B_Cart/BCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.B_Cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
